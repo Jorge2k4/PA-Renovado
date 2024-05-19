6113107880eae8091f64580c37b3fe876909d215
Jorge José Meza Moscote <jm102760@gmail.com>
2024-05-19 16:10:15 -0500

    mensaje

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST["enviar"])) {
+/*if(isset($_POST["enviar"])) {
     if(!empty($_POST['nombre']) && !empty($_POST['asunto']) && !empty($_POST['correo']) && !empty($_POST['msg'])) {
     $name = $_POST['nombre'];
     $email = $_POST['correo'];
@@ -22,7 +22,70 @@ if(isset($_POST["enviar"])) {
         echo "<h4>Mensaje enviado exitosamente</h4>";
     }
     }
+}*/
+
+
+
+// Declarando variables 
+//----------------------------------------------------
+$name = $_POST['nombre'];
+$email = $_POST['correo'];
+$m_subject = $_POST['asunto'];
+$message = $_POST['msg'];
+//-----------------------------------------------------
+
+if (isset($_POST["enviar"])) {
+    $isValid = true;
+
+    // Validar nombre
+    if (empty($_POST['nombre'])) {
+        $isValid = false;
+        echo "Error: Ingrese su nombre.";
+    }
+
+    // Validar correo electrónico
+    if (empty($_POST['correo']) || !filter_var($_POST['correo'], FILTER_VALIDATE_EMAIL)) {
+        $isValid = false;
+        echo "Error: Ingrese una dirección de correo electrónico válida.";
+    }
+
+    // Validar asunto
+    if (empty($_POST['asunto'])) {
+        $isValid = false;
+        echo "Error: Ingrese un asunto.";
+    }
+
+    // Validar mensaje
+    if (empty($_POST['msg'])) {
+        $isValid = false;
+        echo "Error: Ingrese un mensaje.";
+    }
+
+    // Si la validación pasa, proceda a enviar el correo electrónico
+    if ($isValid) {
+        $header = "From: $email" . "\r\n";
+        $header.= "Reply-To: $to" . "\r\n";
+        $header.= "Content-Type: text/plain; charset=UTF-8" . "\r\n"; // Establecer la codificación de caracteres para una visualización adecuada
+        $header.= "X-Mailer: PHP/" . phpversion();
+    
+        $to = "marinaexpressoficial@gmail.com"; // Reemplace con su correo electrónico de destino
+        $subject = "$m_subject: $nombre";
+    
+        $body = "Ha recibido un nuevo mensaje del formulario de contacto de su sitio web.\n\n" .
+                "Aquí están los detalles:\n\n" .
+                "Nombre: $nombre\n\n" .
+                "Correo electrónico: $email\n\n" .
+                "Asunto: $m_subject\n\n" .
+                "Mensaje: $message";
+    
+        if (@mail($to, $subject, $body, $header)) {
+            echo "<h4>Mensaje enviado exitosamente</h4>";
+        } else {
+            echo "Error: Hubo un problema al enviar el correo electrónico. Inténtalo de nuevo más tarde.";
+        }
+    }
 }
+
 
 
 ?>
--- contactanos.php
+++ contactanos.php
@@ -136,7 +136,8 @@
                 <div class="col-lg-8">
                     <div class="contact-form bg-white" style="padding: 30px;">
                         <div id="success"></div>
-                        <form name="sentMessage" id="contactForm" novalidate="novalidate" method="post">
+                        <!-- FORMULARIO DE ENVIO DEL MENSAJE -->
+                        <form name="sentMessage" id="contactForm" novalidate="novalidate" method="post" action="contact.php">
                             <div class="form-row">
                                 <!-- AQUI VA EL NOMBRE DE LA PERSONA -->
                                 <div class="control-group col-sm-6">
